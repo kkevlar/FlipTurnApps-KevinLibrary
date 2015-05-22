1d8400e65b3bd84b9edbba8654dda0de4752a2a9
flipturnapps <flipturnapps@gmail.com>
2015-05-22 17:09:15 +0800

    Changed BasicCommandParser

--- src/com/flipturnapps/kevinLibrary/command/BasicCommandParser.java
+++ src/com/flipturnapps/kevinLibrary/command/BasicCommandParser.java
@@ -6,23 +6,22 @@ public class BasicCommandParser extends CharacterBrokenCommandParser {
 
 	public BasicCommandParser(ArrayList<Command> commands) {
 		super(commands);
-		// TODO Auto-generated constructor stub
+		 
 	}
 
 	public BasicCommandParser(Command[] commands) {
 		super(commands);
-		// TODO Auto-generated constructor stub
+		 
 	}
 
 	@Override
 	public String getSplittingCharacters() {
-		// TODO Auto-generated method stub
+		
 		return " ";
 	}
 
 	@Override
 	public String getStartingCharacters() {
-		// TODO Auto-generated method stub
 		return "";
 	}
 
